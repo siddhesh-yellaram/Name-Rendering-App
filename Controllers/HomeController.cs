c81c31f5f34b94ab3dcb488604a4d74015002766
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 02:29:45 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NameRenderingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Welcome
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+    }
+}
